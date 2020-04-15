02ec0f41df2ef2e166e8859a8f2e8efbf692d60f
samir <mohd.samiur123@gmail.com>
2020-04-15 13:12:13 +0530

    changes

--- app/src/main/java/com/example/spar/Splash_Screen_Activity.java
+++ app/src/main/java/com/example/spar/Splash_Screen_Activity.java
@@ -10,7 +10,7 @@ import android.view.WindowManager;
 
 public class Splash_Screen_Activity extends AppCompatActivity
 {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 3500;
 
 
     @Override
